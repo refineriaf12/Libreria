6d3db9c4907a78b4c4ba6d2ecbb3313799c15e46
Javier Olano <javiolanojol@gmail.com>
2021-01-12 19:08:37 +0100

    Imagen estatica y eliminacion de json

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,12 +2,11 @@ const express = require('express');
 const router = express.Router();
 const user = require('../controllers/userController');
 const controller = require('../controllers/generalController');
-const welcome = require('../data/bienvenidos.json');
 
 
 router.get('/' ,(req,res)=>{
 
-    res.render('templates/loginTemplate',{user_img:welcome});
+    res.render('templates/loginTemplate');
 
 
 });
